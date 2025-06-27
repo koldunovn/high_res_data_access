303a84d6ff03d15939ab9930847a71a647a76f44
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-06-27 17:14:17 +0300

    job adapted to complete LRA 1 2d variable for the three available experiments

--- aqua-scripts/launch.sh
+++ aqua-scripts/launch.sh
@@ -6,12 +6,12 @@
 #SBATCH --account=project_462000911
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
-#SBATCH --time=02:00:00
-#SBATCH --mem=200G
+#SBATCH --time=08:00:00
+#SBATCH --mem=64G
 set -e
 
 var=2t
-catalog=climatedt-phase1
+catalog=data-access
 model=IFS-FESOM
 exps=(story-2017-historical-bridge story-2017-historical-HPC story-2017-historical-stac)
 source=hourly-hpz9-atm2d
@@ -22,5 +22,7 @@ tmpdir=/scratch/project_462000911/mnurisso/lra_tmp
 
 for exp in "${exps[@]}"; do
     echo "Processing experiment: $exp"
-    python lra_prec_italy.py --var $var --catalog $catalog --model $model --exp $exp --source $source --freq $freq --regrid $regrid
+    python lra-cli.py --var $var --catalog $catalog --model $model --exp $exp --source $source --freq $freq --regrid $regrid --outdir $outdir --tmpdir $tmpdir
+    echo "Completed processing for experiment: $exp"
+    echo "----------------------------------------"
 done
