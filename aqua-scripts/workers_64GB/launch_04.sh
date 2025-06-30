6fc170c8bfb2cf0d2d29f3021341c08498274968
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-06-30 17:55:44 +0300

    test for various worker/memory setup

---
+++ aqua-scripts/workers_64GB/launch_04.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=small
+#SBATCH --job-name=data-access_04
+#SBATCH --output=data-access_04_%j.out
+#SBATCH --error=data-access_04_%j.err
+#SBATCH --account=project_462000911
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=08:00:00
+#SBATCH --mem=64G
+set -e
+
+nproc=4
+var=2t
+catalog=data-access
+model=IFS-FESOM
+exp=story-2017-historical-HPC
+source=hourly-hpz9-atm2d
+freq=monthly
+regrid=r100
+outdir=/scratch/project_462000911/mnurisso/data-access-workers/$nproc
+mkdir -p $outdir
+tmpdir=/scratch/project_462000911/mnurisso/lra_tmp
+
+python lra-cli.py --var $var --catalog $catalog --model $model --exp $exp --source $source --freq $freq --regrid $regrid --outdir $outdir --tmpdir $tmpdir --nproc $nproc
