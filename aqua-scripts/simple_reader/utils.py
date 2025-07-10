fbf6b9fdbdab78a6608b17da30d5c21c64009310
mnurisso <m.nurisso@isac.cnr.it>
2025-07-10 16:28:28 +0200

    update for possible chunk in filename

--- aqua-scripts/simple_reader/utils.py
+++ aqua-scripts/simple_reader/utils.py
@@ -34,12 +34,22 @@ def read_benchmark_file(filepath):
 
     # Extract metadata from filename
     match = re.search(r'results_(\w+?)_(\d+)workers_(\d+)GB\.txt', filepath)
+    match2 = re.search(r'results_(\w+?)_(\d+)workers_(\d+)GB_([6h|3h|D|W|h]+)chunk\.txt', filepath)
     if match:
         varname, nworkers, mem_gb = match.groups()
         for r in records:
             r['Variable'] = varname
             r['Workers'] = int(nworkers)
             r['Memory (GB)'] = int(mem_gb)
+    elif match2:
+        varname, nworkers, mem_gb, chunk_size = match2.groups()
+        for r in records:
+            r['Variable'] = varname
+            r['Workers'] = int(nworkers)
+            r['Memory (GB)'] = int(mem_gb)
+            r['Chunk Size'] = str(chunk_size)
+    else:
+        raise ValueError(f"Filename '{filepath}' does not match expected pattern.")
 
     return pd.DataFrame(records)
 
