c40a5ec4965924abae72a176ff001d8e8a8ee4ab
mnurisso <m.nurisso@isac.cnr.it>
2025-07-18 12:24:24 +0200

    Workers and Chunk scaling

--- aqua-scripts/simple_reader/__init__.py
+++ aqua-scripts/simple_reader/__init__.py
@@ -1,5 +1,5 @@
 from .plot import plot_normalized_scaling
-from .utils import read_benchmark_file, load_all_benchmarks
+from .utils import read_benchmark_file, load_all_benchmarks, chunk_translation
 
 __all__ = ['plot_normalized_scaling',
-           'read_benchmark_file', 'load_all_benchmarks']
+           'read_benchmark_file', 'load_all_benchmarks', 'chunk_translation']
