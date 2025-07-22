e716a4829545e9bf8e581cbc273f445902c1c10e
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-07-22 16:46:45 +0300

    smmregrid pure comparison added

--- aqua-scripts/regrid/regrid-comparison.py
+++ aqua-scripts/regrid/regrid-comparison.py
@@ -1,8 +1,10 @@
 import argparse
 import os
+import gc
 import time
 import xarray as xr
 from cdo import Cdo
+from smmregrid import Regridder
 from aqua import Reader
 from aqua.logger import log_configure
 from aqua.diagnostics.core import open_cluster, close_cluster
@@ -50,6 +52,9 @@ def compute_cdo(filepath: str, weightspath:str, targetgridfile: str, outdir: str
 
     total_time = time2 - start_time
     logger.info(f"Total time taken: {total_time:.2f} seconds")
+
+    del output
+    gc.collect()
 
     return compute_time, total_time
 
@@ -99,7 +104,63 @@ def compute_aqua(catalog: str, model: str, exp: str, source: str, nproc: int,
     total_time = time3 - start_time
     logger.info(f"Total time taken: {total_time:.2f} seconds")
 
+    del data_regridded
+    gc.collect()
+
     return retrieve_time, compute_time, io_time, total_time
+
+def compute_smmregrid(input_file, weights_file,
+                      outdir: str = './', loglevel: str = 'INFO'):
+    """
+    Compute the regrid using smmregrid
+    It times:
+        - Time taken to compute data
+        - Time taken to I/O the data
+        - Total time
+
+    Args:
+        input_file (str): file to regrid
+        weights_file (str): path of the weights
+        outdir (str, opt): where to store the regridded file
+        loglevel (str): Logging level. Defaults to 'INFO'.
+    """
+    logger = log_configure(log_level=loglevel, log_name='RegridComparison-smmregrid')
+    start_time = time.time()
+    logger.info(f"Start time: {start_time:.2f}")
+
+    xfield = xr.open_mfdataset(input_file, chunks={"time": 24})
+    wfield = xr.open_mfdataset(weights_file)
+
+    time1 = time.time()
+    setup_time = time1 - start_time
+    logger.info(f"Dataset setup time: {setup_time:.2f}")
+
+    interpolator = Regridder(weights=wfield)
+
+    data_regridded = interpolator.regrid(xfield)
+    data_regridded.load()
+
+    time2 = time.time()
+    compute_time = time2 - time1
+    logger.info(f"Compute time: {compute_time:.2f}")
+
+    filename = f"smmregrid_regridded_{int(time.time())}.nc"
+    filename = os.path.join(outdir, filename)
+
+    data_regridded.to_netcdf(filename)
+
+    time3 = time.time()
+    io_time = time3 - time2 + setup_time
+    logger.info(f"I/O time (setup + write): {io_time:.2f}")
+
+    total_time = time3 - start_time
+    logger.info(f"Total time: {total_time:.2f}")
+
+    del data_regridded
+    gc.collect()
+
+    return compute_time, io_time, total_time
+
 
 if __name__ == '__main__':
 
@@ -121,6 +182,7 @@ if __name__ == '__main__':
     filepath = '/scratch/project_462000911/mnurisso/data-access/regrid-catalog/IFS-FESOM_story-2017-historical-HPC_hourly-hpz9-atm2d_tprate_20180101T0000-20180131T2300.nc'
     weightspath = '/appl/local/climatedt/data/AQUA/weights/weights_hpz9-nested_ycon_r100_l2d.nc'
     targetgridfile = './r360x180.nc'
+    repetitions = 3
 
     if nproc > 1:
         logger.info(f"Opening a cluster with {nproc} workers")
@@ -128,15 +190,50 @@ if __name__ == '__main__':
     else:
         logger.info("No dask cluster is opened, using local Dask client")
 
-    if nproc == 1:
-        cdo_compute_time, cdo_total_time = compute_cdo(
-            filepath=filepath, weightspath=weightspath, targetgridfile=targetgridfile,
-            loglevel=loglevel
-        )
+    # Initialize results to zero
+    cdo_compute_time = cdo_total_time = 0
+    retrieve_time = compute_time = io_time = total_time = 0
+    smm_compute_time = smm_io_time = smm_total_time = 0
 
-    retrieve_time, compute_time, io_time, total_time = compute_aqua(
-        catalog=catalog, model=model, exp=exp, source=source, nproc=nproc, loglevel=loglevel
-    )
+    for i in range(repetitions):
+
+        if nproc == 1:
+            cdo_ct, cdo_tt = compute_cdo(
+                filepath=filepath, weightspath=weightspath, targetgridfile=targetgridfile,
+                loglevel=loglevel, outdir=outdir
+            )
+            cdo_compute_time += cdo_ct
+            cdo_total_time += cdo_tt
+
+        r_time, c_time, i_time, t_time = compute_aqua(
+            catalog=catalog, model=model, exp=exp, source=source, nproc=nproc,
+            loglevel=loglevel, outdir=outdir
+        )
+        retrieve_time += r_time
+        compute_time += c_time
+        io_time += i_time
+        total_time += t_time
+
+        smm_ct, smm_io, smm_tt = compute_smmregrid(
+            input_file=filepath, weights_file=weightspath, outdir=outdir, loglevel=loglevel
+        )
+        smm_compute_time += smm_ct
+        smm_io_time += smm_io
+        smm_total_time += smm_tt
+
+    # Average over repetitions
+    if repetitions > 0:
+        if nproc == 1:
+            cdo_compute_time /= repetitions
+            cdo_total_time /= repetitions
+
+        retrieve_time /= repetitions
+        compute_time /= repetitions
+        io_time /= repetitions
+        total_time /= repetitions
+        smm_compute_time /= repetitions
+        smm_io_time /= repetitions
+        smm_total_time /= repetitions
 
     results_file = './regrid_comparison_results.yaml'
     if os.path.exists(results_file):
@@ -150,6 +247,11 @@ if __name__ == '__main__':
             'compute_time': compute_time,
             'io_time': io_time,
             'total_time': total_time
+        },
+        f'smmregrid_{nproc}': {
+            'compute_time': smm_compute_time,
+            'io_time': smm_io_time,
+            'total_time': smm_total_time
         }
     }
     if nproc == 1:
