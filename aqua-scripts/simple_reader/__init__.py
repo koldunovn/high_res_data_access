f06b8b47693c85a7c9a4916aeccfb61b32a5c35a
mnurisso <m.nurisso@isac.cnr.it>
2025-10-22 22:15:27 +0200

    utility update

--- aqua-scripts/simple_reader/__init__.py
+++ aqua-scripts/simple_reader/__init__.py
@@ -1,5 +1,7 @@
 from .plot import plot_normalized_scaling
-from .utils import read_benchmark_file, load_all_benchmarks, chunk_translation
+from .utils import read_benchmark_file, load_all_benchmarks
+from .utils import chunk_translation, chunk_translation_reverse
 
 __all__ = ['plot_normalized_scaling',
-           'read_benchmark_file', 'load_all_benchmarks', 'chunk_translation']
+           'read_benchmark_file', 'load_all_benchmarks',
+           'chunk_translation', 'chunk_translation_reverse']
