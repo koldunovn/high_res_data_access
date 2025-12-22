d3ea2d2040798267497b30c9bd24b1ae6cb170a3
mnurisso <m.nurisso@isac.cnr.it>
2025-12-22 12:28:42 +0100

    update installation script

--- aqua-scripts/install-aqua.sh
+++ aqua-scripts/install-aqua.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# AQUA=/path/to/aqua
 machine=lumi
 
-yes | aqua install $machine -e $AQUA/config
+# We install aqua (can be installed interactively with --core path/to/AQUA)
+yes | aqua install $machine
 
-# modify according to your folder structure
+# Modify according to your folder structure
+# There is a catalog specific to this data access experiment
 CATDIR=$AQUA/../high_res_data_access/aqua_catalog
 
 # let's install the data access catalog
@@ -13,15 +14,15 @@ aqua add data-access -e $CATDIR
 # If you need to install any other aqua catalog to not disrupt your current installation
 # you can do it here, by default we install the catalogs useful on lumi
 
-# modify according to your folder structure
-AQUA_CATALOG=$AQUA/../Climate-DT-catalog
+# Modify according to your folder structure
+# AQUA_CATALOG=$AQUA/../Climate-DT-catalog
 
-# we loop over the catalogs and install them
-aqua add obs -e $AQUA_CATALOG/catalogs/obs
-aqua add climatedt-phase1 -e $AQUA_CATALOG/catalogs/climatedt-phase1
-aqua add lumi-phase1 -e $AQUA_CATALOG/catalogs/lumi-phase1
-aqua add lumi-phase2 -e $AQUA_CATALOG/catalogs/lumi-phase2
-aqua add nextgems4 -e $AQUA_CATALOG/catalogs/nextgems4
-aqua add climatedt-e25.1 -e $AQUA_CATALOG/catalogs/climatedt-e25.1
-aqua add climatedt-o25.1 -e $AQUA_CATALOG/catalogs/climatedt-o25.1
-aqua add ci -e $AQUA_CATALOG/catalogs/ci
+# Uncomment the following lines to install additional catalogs
+# aqua add obs -e $AQUA_CATALOG/catalogs/obs
+# aqua add climatedt-phase1 -e $AQUA_CATALOG/catalogs/climatedt-phase1
+# aqua add lumi-phase1 -e $AQUA_CATALOG/catalogs/lumi-phase1
+# aqua add lumi-phase2 -e $AQUA_CATALOG/catalogs/lumi-phase2
+# aqua add nextgems4 -e $AQUA_CATALOG/catalogs/nextgems4
+# aqua add climatedt-e25.1 -e $AQUA_CATALOG/catalogs/climatedt-e25.1
+# aqua add climatedt-o25.1 -e $AQUA_CATALOG/catalogs/climatedt-o25.1
+# aqua add ci -e $AQUA_CATALOG/catalogs/ci
